cb03bb1bbd6da8a39b05d1760dc819c0ce20db92
Zach Baird <zach.d.baird@leidos.com>
2020-02-26 15:31:52 -0500

    Initial commit of server files.

---
+++ TodoTracker/Controllers/TodoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoTracker.Models;
+
+namespace TodoTracker.Controllers
+{
+    [ApiController]
+    [Route("[controller]", Name = "todo")]
+    public class TodoController : ControllerBase
+    {
+        List<Todo> _todos = new List<Todo>()
+        {
+            new Todo { Id = 1, Name = "Complete Todo app", Status = Status.Dev, DateCreated = DateTime.Now, DateFinished = null },
+            new Todo { Id = 2, Name = "Complete Pet Profile app", Status = Status.Backlog, DateCreated = DateTime.Now, DateFinished = null }
+        };
+
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            return _todos.ToArray(); 
+        }
+
+        [HttpPost]
+        public IEnumerable<Todo> Post(Todo newTodo)
+        {
+            _todos.Add(newTodo);
+
+            return _todos.ToArray();
+        }           
+    }
+}
---
+++ TodoTracker/Models/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoTracker.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Status Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateFinished { get; set; }
+
+    }
+
+    public enum Status
+    {
+        Backlog,
+        Committed,
+        Dev,
+        Deploy,
+        Finished
+    }
+}
